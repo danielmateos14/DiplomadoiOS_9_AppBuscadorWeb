e2a62a6bc5fdac1722d7f06062a5b10d481a1bb0
djdenielb <luisdmateos@outlook.com>
2022-04-22 18:03:33 -0500

    Initial Commit

---
+++ 9AppBuscador/Controlador/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9AppBuscador
+//
+//  Created by djdenielb on 22/04/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
